4df0a05530a6ac7fe5d9369600ac577c2361c4d0
HaoChunYang <lanianer@qq.com>
2021-07-13 00:32:54 +0800

    01_parse-10-64

---
+++ 01_parse-10-64/parse64.js
@@ -0,0 +1,22 @@
+const char64 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+                'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+                'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 
+                'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
+                'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 
+                'Y', 'Z', '+', '~', ]
+
+const originNum = 123456789
+
+function encode (num10) {
+  let num64
+
+
+  return num64
+}
+
+function decode (str64) {
+  let num10
+
+  return num10
+}
